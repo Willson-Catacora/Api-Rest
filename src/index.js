2f39402aca8b18044e3c838cb69cd8e4eff5cdb2
willson <willsoncatacora1@gmail.com>
2024-01-19 13:11:32 -0400

    Inicializando el Proyecto - Configuraciones Basicas

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PUERTO = process.env.PORT || 3000;
+
+app.get('/', (req, res) =>{
+    res.send("<h2>It's Working!</h2>");
+});
+
+app.listen(PUERTO, () => {
+    console.log(`El servidor esta escuchando en el puerte ${PUERTO}...`)
+})
