8b01a01a7ef94a6d5a3de8110bcd49c5153cc47a
willson <willsoncatacora1@gmail.com>
2024-01-19 16:48:51 -0400

    Se anadio el archivo workoutService y se modifico el workoutController.

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,16 +1,23 @@
+const workoutService = require('../services/workoutService');
+
 const getAllWorkouts = (req , res) => {
+    const allWorkouts = workoutService.getAllWorkouts();
     res.send('Get all workouts');
 };
 const getOneWorkout = (req , res) => {
+    const workout = workoutService.getOneWorkout();
     res.send('Get an exidting workout');
 };
 const createNewWorkout = (req , res) => {
+    const createdWorkout = workoutService.createNewWorkout();
     res.send('Create an new workout');
 };
 const updateOneWorkout = (req , res) => {
+    const updatedWorkout = workoutService.updateOneWorkout();
     res.send('Update an new workout');
 };
 const deleteOneWorkout = (req , res) => {
+    workoutService.deleteOneWorkout();
     res.send('Delete an existing workout');   
 };
 
